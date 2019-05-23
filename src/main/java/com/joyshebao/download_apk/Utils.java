00059d1453467b7ff21b965f4c205a49c0b50dd1
zhangpengju <13290505500@163.com>
2019-05-23 10:16:29 +0800

    提交测试类

---
+++ src/main/java/com/joyshebao/download_apk/Utils.java
@@ -0,0 +1,14 @@
+package com.joyshebao.download_apk;
+
+import android.util.Log;
+
+/**
+ * Created by zhangpengju on 2019/5/23.
+ */
+
+public class Utils {
+
+    public static void testUtils(){
+        Log.e(Utils.class.getSimpleName(),"-------------------------------->");
+    }
+}
